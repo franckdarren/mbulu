68b4d298fe4b10d3e4d5482e6f69d5c0791e4cd6
mando <mandochrist13@gmail.com>
2024-07-18 09:43:36 +0100

    intégration composant header cours

--- app/cours/Header.jsx
+++ app/cours/Header.jsx
@@ -1,7 +1,16 @@
-export function Header() {
+export function Header({Image1, titreParagraph, titreText, Image2}) {
     return (
-        <div className="">
-            <p>Header</p>
+        <div className="md:px-[70px] md:py-9 ">
+            <div class="md:flex md:w-[100%] flex-row-reverse md:mb-10 md:h-[507px] h-[350px] md:justify-around md:w:[100%] justify-center items-center flex px-5">
+                <div class="md:w-[500px]"> <img src={Image1} alt="" srcset="" /> </div>
+                <div>
+                    <div class="md:w-[237px] w-[137px]"> <img src={Image2} alt="" srcset="" /> </div>
+                    <div class="text-[48px] md:text-[68px] text-center">{titreText}</div>
+                </div>
+            </div>
+            <div className="bg-[#fb9600] p-5 rounded-[20px]">
+                <p class="text-[20px]">{titreParagraph}</p>
+            </div>
         </div>
     );
 }
--- app/cours/page.js
+++ app/cours/page.js
@@ -8,7 +8,7 @@ export default function PageCours() {
     return (
         <main className="">
             <Navbar />
-            <Header />
+            <Header Image2="/assets/punu.png"  Image1="/assets/mot.gif" titreText="Punu" titreParagraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute  eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." />
             <CoursListe />
             <Footer />
         </main>
