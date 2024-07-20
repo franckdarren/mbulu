d6c1a3ecb82ecd2b4576a9034794fa16f6cb7d57
Franck Darren <kikondanze@gmail.com>
2024-07-20 09:07:17 +0100

    fix bug navbar

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -18,34 +18,8 @@ export function Navbar() {
                             className="object-cover h-[50px] w-auto"
                         />
                     </a>
-                    <div className="flex gap-3">
-                        <Button titreButton="S'inscrire" color="bg-[#22AAC6] hover:bg-[#e08e49]" />
-                        <Button titreButton="Se connecter" color="bg-[#093741] hover:bg-[#e08e49]" />
-                    </div>
-                    <button
-                        data-collapse-toggle="navbar-default"
-                        type="button"
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                        aria-controls="navbar-default"
-                        aria-expanded="false"
-                    >
-                        <span className="sr-only">Open main menu</span>
-                        <svg
-                            className="w-5 h-5"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 17 14"
-                        >
-                            <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M1 1h15M1 7h15M1 13h15"
-                            />
-                        </svg>
-                    </button>
+
+
                     <div
                         className="hidden w-full md:block md:w-auto"
                         id="navbar-default"
@@ -78,7 +52,34 @@ export function Navbar() {
                             </li>
                         </ul>
                     </div>
-
+                    <div className="flex gap-3">
+                        <Button titreButton="S'inscrire" color="bg-[#22AAC6] hover:bg-[#e08e49]" />
+                        <Button titreButton="Se connecter" color="bg-[#093741] hover:bg-[#e08e49]" />
+                    </div>
+                    <button
+                        data-collapse-toggle="navbar-default"
+                        type="button"
+                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        aria-controls="navbar-default"
+                        aria-expanded="false"
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <svg
+                            className="w-5 h-5"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 17 14"
+                        >
+                            <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M1 1h15M1 7h15M1 13h15"
+                            />
+                        </svg>
+                    </button>
                 </div>
             </nav>
         </div>
