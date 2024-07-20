fb3b4843b491d988eb537cc219fc70ed8a65f254
jeanclaude sperandezi <sounguejc@gmail.com>
2024-07-20 04:39:31 +0100

    finish section card mask dictionaire et section info

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { Navbar } from "./_components/Navbar";
 import { Footer } from "./_components/Footer";
 import {Button} from "./_components/Button";
+import { MasqueCard } from "./_components/MasqueCard";
+import { workVideo } from "./_components/MasqueCard";
 
 export default function Home() {
   return (
@@ -11,6 +13,7 @@ export default function Home() {
       <Section1 />
       <Section2 />
       <Section3 />
+      <SectionCard />
       <Section4 />
       <Footer />
     </main>
@@ -45,9 +48,42 @@ const Section2 = () => {
   );
 };
 
-const Section3 = () => {
+const Section3= () => {
   return (
-    <p>Section 3</p>
+      <div className="bg-[#03829c]"> 
+       <Image src ="/assets/WeWork — Rebecca Mock.gif"
+            width={185}
+            height={153}
+            className="relative left-[210px] top-[15px]  rounded-lg md:w-[284px] md:h-[152px] md:left-[60%]"
+        />
+    
+      <h3 className="my-[15px] text-center text-[#fb9600] font-semibold text-[23px] md:text-[30px]">Obtenez des traduction<br/> instantanées dans plusieurs<br/>  langues gabonaises</h3>
+      
+      <div className="flex flex-row justify-evenly items-center ">
+         <Image src ="/assets/Keyword search.gif"
+            width={185}
+            height={153}
+            className=" rounded-lg  mb-[10px] md:w-[207px] md:h-[150px]"
+          />
+
+        <Button titreButton="Essayer une traduction" color="bg-[#FB9600] hover:bg-[#f7af44]"/>
+      </div>
+    </div>
+  );
+};
+
+const SectionCard = () => {
+  return (
+    <div>
+      <h1 className="text-center text-[16px] font-normal md:text-[20px] md:font-normal md: my-[25px]">Choisir la langue</h1>
+      <div className="flex flex-col items-center mt-[10px] mb-[10px] md:flex md:flex-row md:items-center md:justify-center md:gap-[15px]">
+
+        <MasqueCard titre = "Fang" image = "/assets/masque-fang.png"/>
+        <MasqueCard titre = "Punu" image = "/assets/masque-punu.png"/>
+        <MasqueCard titre= "Myene" image = "/assets/masque-myene.png"/>
+      
+      </div>
+    </div>
   );
 };
 
