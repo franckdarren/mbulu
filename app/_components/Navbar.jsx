91dbb0d28000c0bfd8ce414274aa5d0cfd563343
Franck Darren <kikondanze@gmail.com>
2024-07-20 09:29:24 +0100

    fix bug navbar

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -18,6 +18,30 @@ export function Navbar() {
                             className="object-cover h-[50px] w-auto"
                         />
                     </a>
+                    <button
+                        data-collapse-toggle="navbar-default"
+                        type="button"
+                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        aria-controls="navbar-default"
+                        aria-expanded="false"
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <svg
+                            className="w-5 h-5"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 17 14"
+                        >
+                            <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M1 1h15M1 7h15M1 13h15"
+                            />
+                        </svg>
+                    </button>
 
 
                     <div
@@ -50,36 +74,30 @@ export function Navbar() {
                                     A propos
                                 </a>
                             </li>
+                            <li>
+                                <a
+                                    href="/register"
+                                    className="md:hidden block py-2 px-3 text-white rounded  md:border-0 md:hover:text-[#22AAC6] md:p-0 "
+                                >
+                                    S&apos;inscrire
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="/login"
+                                    className="md:hidden block py-2 px-3 text-white rounded  md:border-0 md:hover:text-[#22AAC6] md:p-0 "
+                                >
+                                    Se connecter
+                                </a>
+                            </li>
                         </ul>
                     </div>
-                    <div className="flex gap-3">
+
+                    <div className="md:flex md:gap-3 hidden">
                         <Button titreButton="S'inscrire" color="bg-[#22AAC6] hover:bg-[#e08e49]" />
                         <Button titreButton="Se connecter" color="bg-[#093741] hover:bg-[#e08e49]" />
                     </div>
-                    <button
-                        data-collapse-toggle="navbar-default"
-                        type="button"
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                        aria-controls="navbar-default"
-                        aria-expanded="false"
-                    >
-                        <span className="sr-only">Open main menu</span>
-                        <svg
-                            className="w-5 h-5"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 17 14"
-                        >
-                            <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M1 1h15M1 7h15M1 13h15"
-                            />
-                        </svg>
-                    </button>
+
                 </div>
             </nav>
         </div>
