e2dbde1255f5ea970dd3f62043fefbe3b8f32a05
jeanclaudesperandezi <sounguejc@gmail.com>
2024-07-22 13:06:52 +0100

    ajout des modifications

--- app/dictionnaire/page.js
+++ app/dictionnaire/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Search } from "./Search";
 import { Navbar } from "../_components/Navbar";
 import { Footer } from "../_components/Footer";
+import { Dictionnaire } from "./Dictionnaire";
 
 export default function PageDictionnaire() {
     return (
@@ -31,6 +32,8 @@ export default function PageDictionnaire() {
                
             </div>
             <div className="bg-[#FB9600] w-full pl-[20px] mt-[30px]">Dictionnaire Français-Punu</div>
+
+            <Dictionnaire titre="Bonjour" expression="Marambura" other="Lorem"/>
 
             <Footer />
         </main>
