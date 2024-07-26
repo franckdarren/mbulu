8a8defa1e364fee836ccfe0ca990afca4279a945
mando_christ <mandochrist13@gmail.com>
2024-07-26 04:22:13 +0200

    amélioration des pages accueil, a propos et cours

--- app/page.js
+++ app/page.js
@@ -54,20 +54,14 @@ const Header = () => {
 
 const Section1 = () => {
   return (
-    <div class=" md:h-[400px] md:mx-[10%] bg-gradient-to-r my-7 from-orange-400 to-orange-500 rounded-xl text-white">
-      <div class="flex justify-center px-1">
-        <div class="md:h-[400px] md:flex md:flex-col md:justify-end md:items-baseline ">
-          <Image
-            src="/assets/___1_-removebg-preview.png"
-            width={212}
-            height={106}
-            alt="image"
-            class=" w-[100%] h-[100%] "
-          />
+
+    <div className=" md:h-[400px] my-[50px] md:my-[66px]  md:mx-[10%] rounded-xl  text-white bg-cover bg-center " style={{ backgroundImage: 'url(/assets/___1_-removebg-preview.png)' }} >
+      <div className="flex rounded-[10px] bg-black bg-opacity-60 shadow-overlay justify-center px-1 relative z-10">
+        <div className="md:h-[400px] md:flex md:flex-col md:justify-end md:items-baseline">
         </div>
-        <div class="md:flex md:flex-col md:justify-center w-[70%] px-4">
-          <h1 class="text-2xl font-bold my-2">Interactive Announcement</h1>
-          <p class="my-2 text-[15px]">
+        <div className="md:flex md:flex-col md:justify-center w-[80%] py-2 px-3">
+          <h1 className="text-2xl font-bold my-2">Interactive Announcement</h1>
+          <p className="my-2 text-[15px]">
             Bienvenue sur Mbulu !
             Nous sommes ravis de vous accueillir sur le premier dictionnaire numérique interactif dédié aux langues gabonaises. Mbulu a été conçu pour préserver et promouvoir la richesse linguistique et culturelle du Gabon. Que vous soyez un jeune Gabonais désireux de reconnecter avec vos racines ou un passionné de langues en quête de nouvelles découvertes, vous êtes au bon endroit.
           </p>
@@ -77,37 +71,41 @@ const Section1 = () => {
   );
 };
 
+
+
 const Section2 = () => {
   return (
-    <div className="bg-[#03829c]">
-      <Image src="/assets/WeWork — Rebecca Mock.gif"
+    <div className="my-[80px] h-[300px] md:h-[400px] lg:h-[450px] lg:px-[10%] px-4 gap-1 bg-center bg-cover flex" style={{ backgroundImage: 'url(/assets/keywordSearch.gif)' }}>
+      {/* <Image src="/assets/WeWork — Rebecca Mock.gif"
         width={145}
         height={113}
         alt="gif"
         className="relative left-[210px] top-[15px]  rounded-lg md:w-[284px] md:h-[152px] md:left-[60%]"
-      />
+      /> */}
 
-      <h3 className="my-[15px] text-center text-[#fb9600] font-semibold text-[23px] md:text-[30px]">Obtenez des traduction<br /> instantanées dans plusieurs<br />  langues gabonaises</h3>
-
-      <div className="flex flex-row justify-evenly items-center ">
-        <Image src="/assets/KeywordSearch.gif"
+        {/* <Image src="/assets/KeywordSearch.gif"
           width={145}
           height={113}
           alt="gif"
-          className=" rounded-lg  mb-[10px] md:w-[207px] md:h-[150px]"
-        />
+          className=" rounded-lg  mb-[10px] h-[150px] md:w-full max-w-[400px] md:h-full"
+        /> */}
+      
+      <div className="flex md:mb-0 mb-3 lg:py-5 flex-col gap-5 md:gap-10 justify-between p-2 items-center ">
+      <h3 className=" text-center text-[#012d36] font-semibold text-[18px] md:text-[30px]">Obtenez des traduction instantanées dans plusieurs langues gabonaises</h3>
+
 
         <Button titreButton="Essayer une traduction" color="bg-[#FB9600] hover:bg-[#f7af44]" />
       </div>
+      
     </div>
   );
 };
 
 const SectionCard = () => {
   return (
-    <div>
-      <h1 className="text-center text-[16px] font-normal md:text-[20px] md:font-normal md: my-[25px]">Choisir la langue</h1>
-      <div className="flex flex-col items-center mt-[10px] mb-[10px] md:flex md:flex-row md:items-center md:justify-center md:gap-[15px]">
+    <div className="bg-[#fb9600] py-5 ">
+      <h1 className="text-center text-white text-[23px] font-extrabold md:text-[30px]  md: my-[25px]">Choisir la langue</h1>
+      <div className="flex flex-col items-center gap-5 mt-[10px] mb-[10px] md:flex md:flex-row md:items-center md:justify-center md:gap-[15px]">
 
         <MasqueCard titre="Fang" image="/assets/masque-fang.png" />
         <MasqueCard titre="Punu" image="/assets/masque-punu.png" />
@@ -120,7 +118,7 @@ const SectionCard = () => {
 
 const Section4 = () => {
   return (
-    <div class="bg-[#012d36] md:flex py-[20px] md:flex-row flex-col items-center md:justify-around px-6">
+    <div class="bg-[#012d36] md:flex py-[20px] mt-[100px] md:flex-row flex-col items-center md:justify-around px-6">
       <div class="md:w-[95%] md:ml-8">
         <div>
           <h1 class="text-[#fb9600] leading-tight text-[20px] md:text-[50px]">Un panel de cours gratuits et adaptés à tous</h1>
@@ -141,7 +139,8 @@ const Section4 = () => {
 
 const SectionFaqs = () => {
   return (
-    <div class="py-9 bg-[#fb9600]">
+    <div class="py-11 bg-[#fb9600]">
+      <h1 class="text-center text-white text-[23px] font-semibold md:text-[30px]  md:my-[25px]">Questions fréquentes</h1>
       <ButtonFaqs titreButtonFaqs="Qu&apos;est ce que MBULU?" paragraphFaqs="MBULU est un dictionnaire destiné à préserver et valoriser les langues et la culture gabonaises. il est conçu pour la jeunesse gabonaise ainsi que pour toute personne souhaitant apprendre une langue gabonaise." />
       <ButtonFaqs titreButtonFaqs="A qui s&apos;adresse MBULU?" paragraphFaqs="Le dictionnaie s&apos;adresse principalement aux jeunes Gabonais âgés de 15 à 25 ans. Mais il est également ouvert à toute personne intéressée par l&apos;apprentissage des langues gabonaises." />
       <ButtonFaqs titreButtonFaqs="Comment puis-je contribuer à MBULU?" paragraphFaqs="Les utilisateurs peuvent suggérer, ajouter et vérifier du contenu en se connectant à leur compte sur la plateforme." />
