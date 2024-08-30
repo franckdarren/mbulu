f5399d58e40e0f2f1a62e752fda5d223013c8b24
Franck Darren <kikondanze@gmail.com>
2024-08-30 10:53:15 +0100

    fix bug

--- app/admin/dashboard/page.js
+++ app/admin/dashboard/page.js
@@ -2,21 +2,16 @@ import AdminTitre from "../../../app/_components/AdminTitre";
 import { getCountContributionFromDatabase } from "../../../services/contributionServices";
 import { getCountUsersFromDatabase } from "../../../services/userServices";
 
-
-
-export function DashboardCard({ titre, counter }) {
-
+// Composant de carte de tableau de bord
+function DashboardCard({ titre, counter }) {
     return (
         <div className="flex flex-col bg-white rounded-xl shadow-lg">
             <div className="px-6 py-8 sm:p-10 sm:pb-6">
                 <div className="grid items-center justify-center w-full grid-cols-1 text-left">
                     <div>
-                        <h2
-                            className="text-lg font-medium tracking-tighter text-gray-600 lg:text-3xl"
-                        >
+                        <h2 className="text-lg font-medium tracking-tighter text-gray-600 lg:text-3xl">
                             {titre}
                         </h2>
-                        {/* <p className="mt-2 text-sm text-gray-500">Suitable to grow steadily.</p> */}
                     </div>
                     <div className="mt-6">
                         <p>
@@ -31,21 +26,20 @@ export function DashboardCard({ titre, counter }) {
     );
 }
 
+// Fonction asynchrone de la page principale
 export default async function Dashboard() {
-    let contributions = await getCountContributionFromDatabase()
-    let users = await getCountUsersFromDatabase()
-
-
+    // Appels à la base de données pour obtenir les compteurs
+    let contributions = await getCountContributionFromDatabase();
+    let users = await getCountUsersFromDatabase();
 
     return (
-        <main className="">
+        <main>
             <AdminTitre titre="Tableau de bord" />
             <div className="grid grid-cols-4 gap-5">
                 <DashboardCard titre="Contributions" counter={contributions} />
                 <DashboardCard titre="Utilisateurs" counter={users} />
                 <DashboardCard titre="Langues" counter="3" />
             </div>
-
         </main>
     );
 }
